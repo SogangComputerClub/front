374cf6b07552c379278a8e289df4d8768e180803
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-01-24 23:45:40 +0900

    Update App.js

--- sgcc_front/src/App.js
+++ sgcc_front/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/login';
 import SignUp from './pages/signUp/signUp';
